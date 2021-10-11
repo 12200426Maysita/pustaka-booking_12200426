a55d2c1775f5440df65f6893288973ad449f49fb
12200426Maysita <12200426@bsi.ac.id>
2021-10-11 14:20:35 +0700

    Tugas Pertemuan 4

---
+++ app/Database/Seeds/PenggunaSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class PenggunaSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+        [
+        'nama'     => 'maysita',
+        'password' => md5('12200426')
+        ],
+        [
+        'nama'      =>'admin',
+        'password'  =>md5('12345')
+        ],
+        [
+        'nama'      =>'12200426',
+        'password'  =>md5('maysitautamifadlina')
+        ]
+        ];
+    $p = new Pengguna();
+    $p->inserBatch();
+    }
+}
